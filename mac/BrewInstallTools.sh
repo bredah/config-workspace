d4e3417578f3263c4475390376a9d23be3a581b6
bredahe <henrique.breda@itau-unibanco.com.br>
2016-08-08 10:07:12 -0300

    Add files

---
+++ mac/BrewInstallTools.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Brew packages
+#
+brew install wget
+brew install rbenv
+brew install cask
+brew install maven
+brew install ant
+brew install gradle
+brew install jmeter
+#
+# Some cask package
+#
+brew cask install dropbox
+brew cask install sublime-text
+brew cask install gimp
+brew cask install google-chrome
+brew cask install firefox
+brew cask install soapui
+brew cask install skype
+brew cask install android-studio
+brew cask install xamarin-studio
+brew cask install skitch
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
